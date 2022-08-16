8f2d611ffd253aa1b0bc30584e9408d3d2a906cd
SmallChungus101 <111381299+SmallChungus101@users.noreply.github.com>
2022-08-16 13:20:36 -0400

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,20 @@
+
+function setup(){
+   createCanvas(400, 400);
+ }
+
+function draw() {
+  rectMode(CENTER);
+ 
+  background(0, 255, 255);
+
+  fill(0, 255, 0);
+  noStroke();
+  rect(200, 300, 500, 200);
+   fill(0);
+  stroke(0);
+  ellipse(200, 200, 50, 50);
+  strokeWeight(10);
+   
+     line(130, 210, 270, 190);
+}
